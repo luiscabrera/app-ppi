5590081e0c1fab1df911823d65f96730601c21a4
luiscabrera <luigi.alfonsocabrera@gmail.com>
2023-03-28 13:47:28 -0300

    Align inputs and selects

--- src/components/card-exchange/index.jsx
+++ src/components/card-exchange/index.jsx
@@ -33,23 +33,31 @@ const CardExchange = ({
   return (
     <div className="card-exchange">
       <div className="container-input-button">
-        <Input label="Amount" value={value} onChange={setValue} />
-        <Select
-          label="From"
-          options={currenciesOptions}
-          onChange={setFrom}
-          value={from}
-        />
-        <IconButton
-          onClick={handleChangeFromTo}
-          icon={<MdCurrencyExchange color="#034EEF" size={18} />}
-        />
-        <Select
-          label="To"
-          options={currenciesOptions}
-          onChange={setTo}
-          value={to}
-        />
+        <div className="div-input">
+          <Input label="Amount" value={value} onChange={setValue} />
+        </div>
+        <div className="div-select">
+          <Select
+            label="From"
+            options={currenciesOptions}
+            onChange={setFrom}
+            value={from}
+          />
+        </div>
+        <div className="div-button">
+          <IconButton
+            onClick={handleChangeFromTo}
+            icon={<MdCurrencyExchange color="#034EEF" size={18} />}
+          />
+        </div>
+        <div className="div-select">
+          <Select
+            label="To"
+            options={currenciesOptions}
+            onChange={setTo}
+            value={to}
+          />
+        </div>
       </div>
       <div className="container-values-info">
         <div className="container-values">
