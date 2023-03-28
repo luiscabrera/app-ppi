d460bcbf5b9955eb5b55f57a61a7574f252a5b8b
luiscabrera <luigi.alfonsocabrera@gmail.com>
2023-03-28 11:16:26 -0300

    Add animation to icon button

--- src/components/icon-button/index.jsx
+++ src/components/icon-button/index.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const IconButton = ({ onClick, icon }) => {
+  const [animate, setAnimate] = useState(false);
+
+  function handleClick() {
+    setAnimate(true);
+    onClick();
+    setTimeout(() => {
+      setAnimate(false);
+    }, 400);
+  }
+
   return (
-    <button onClick={onClick} className="button">
+    <button
+      onClick={handleClick}
+      className={`button ${animate ? "rotate-center" : ""}`}
+    >
       {icon}
     </button>
   );
